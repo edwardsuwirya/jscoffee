8ba7638654d9e1f19420c1e9ff6e89f1931e5945
Edward Suwirya <edward_suwirya@yahoo.com>
2022-05-27 16:17:28 +0700

    Callback Hell Example

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
+/*
+Americano
+1. Buat Order 1 detik
+2. Giling kopi 3 detik
+3. Seduh kopi 2 detik
+4. Penyajian kopi 2 detik
+
+Caffe Latte
+1. Buat Order 1 detik
+2. Giling kopi 3 detik
+3. Hangatkan susu 1.5 detik
+4. Seduh kopi + mix susu 3 detik
+5. Penyajian kopi 2 detik
+ */
+
 const order = (beverageProduction) => {
-    console.log("Order placed : ")
-    beverageProduction();
+    setTimeout(() => {
+        console.log("Order placed : ")
+        beverageProduction();
+    }, 1000);
+};
+
+const americanoProduction = () => {
+    const menuName = "Americano";
+    console.log(menuName);
+    setTimeout(() => {
+        console.log("Proses Giling kopi Selesai");
+        setTimeout(() => {
+            console.log("Proses Seduh kopi Selesai");
+            setTimeout(() => {
+                console.log(`Penyajian kopi ${menuName} Selesai`);
+            }, 2000)
+        }, 2000)
+    }, 3000);
 };
 
 const caffeLatteProduction = () => {
-    console.log("Caffe Latte")
+    const menuName = "Caffe Latte";
+    console.log(menuName);
+    setTimeout(() => {
+        console.log("Proses Giling kopi Selesai");
+        setTimeout(() => {
+            console.log("Proses Susu Hangat Selesai");
+            setTimeout(() => {
+                console.log("Proses Seduh kopi,mix Susu Hangat Selesai");
+                setTimeout(() => {
+                    console.log(`Penyajian kopi ${menuName} Selesai`);
+                }, 2000)
+            }, 3000)
+        }, 1500)
+    }, 3000);
 };
 
 order(caffeLatteProduction);
+order(americanoProduction);
 
 
