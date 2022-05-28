e830016eb76c9102135ef99d088ffd078c2553e2
Edward Suwirya <edward_suwirya@yahoo.com>
2022-05-28 11:29:01 +0700

    Promise

--- index.js
+++ index.js
@@ -13,45 +13,48 @@ Caffe Latte
 5. Penyajian kopi 2 detik
  */
 
-const order = (beverageProduction) => {
-    setTimeout(() => {
-        console.log("Order placed : ")
-        beverageProduction();
-    }, 1000);
+let is_open = true;
+
+let job = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_open) {
+            setTimeout(() => {
+                resolve(work())
+            }, time)
+        } else {
+            reject("Our shop is closed");
+        }
+    })
+}
+
+const order = (orders) => {
+    job(1000, () => {
+        console.log("Order placed : ");
+        for (let i = 0; i < orders.length; i++) {
+            orders[i]();
+        }
+    }).catch((err) => console.log(err));
 };
 
 const americanoProduction = () => {
     const menuName = "Americano";
     console.log(menuName);
-    setTimeout(() => {
-        console.log("Proses Giling kopi Selesai");
-        setTimeout(() => {
-            console.log("Proses Seduh kopi Selesai");
-            setTimeout(() => {
-                console.log(`Penyajian kopi ${menuName} Selesai`);
-            }, 2000)
-        }, 2000)
-    }, 3000);
+    job(3000, () => console.log("Proses Giling kopi Selesai"))
+        .then(() => job(2000, () => console.log("Proses Seduh kopi Selesai")))
+        .then(() => job(2000, () => console.log(`Penyajian kopi ${menuName} Selesai`)))
+        .finally(() => console.log("Finish"));
 };
 
 const caffeLatteProduction = () => {
     const menuName = "Caffe Latte";
     console.log(menuName);
-    setTimeout(() => {
-        console.log("Proses Giling kopi Selesai");
-        setTimeout(() => {
-            console.log("Proses Susu Hangat Selesai");
-            setTimeout(() => {
-                console.log("Proses Seduh kopi,mix Susu Hangat Selesai");
-                setTimeout(() => {
-                    console.log(`Penyajian kopi ${menuName} Selesai`);
-                }, 2000)
-            }, 3000)
-        }, 1500)
-    }, 3000);
+    job(3000, () => console.log("Proses Giling kopi Selesai"))
+        .then(() => job(1500, () => console.log("Proses Susu Hangat Selesai")))
+        .then(() => job(3000, () => console.log("Proses Seduh kopi,mix Susu Hangat Selesai")))
+        .then(() => job(2000, () => console.log(`Penyajian kopi ${menuName} Selesai`)))
+        .finally(() => console.log("Finish"));
 };
 
-order(caffeLatteProduction);
-order(americanoProduction);
+order([caffeLatteProduction, americanoProduction]);
 
 
